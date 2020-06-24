a1f4614c5fd6b5e401736f79b764f8c5126bc216
Vandita Attal <vandita.attal@gmail.com>
2020-06-24 15:23:13 +0800

    done a lot of things

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # index, create, new, show
+  resources :restaurants, only: [ :index, :create, :new, :show ] do
+    resources :reviews, only: [ :new, :create ]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+mcdonalds = Restaurant.create(name: 'mcdonalds', address: '1 road road', category: 'french')
+kfc = Restaurant.create(name: 'kfc', address: '1 road road', category: 'belgian')
+mosburger = Restaurant.create(name: 'mosburger', address: '1 road road', category: 'japanese')
+btk = Restaurant.create(name: 'boon tong kee', address: '1 east coast road', category: 'chinese')
+pastafresca = Restaurant.create(name: 'pasta fresca di salvatore', address: '1 upp changi road', category: 'italian')
